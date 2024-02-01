0962fcea4f8c378fd8f3d60f8c443367b9ac3bff
amfuma <anthony.fumagalli88@gmail.com>
2024-02-01 08:29:28 -0600

    potentially finished assigned endpoints

--- src/main/java/com/cooksys/social_media/mappers/TweetMapper.java
+++ src/main/java/com/cooksys/social_media/mappers/TweetMapper.java
@@ -4,10 +4,11 @@ import com.cooksys.social_media.dtos.TweetRequestDto;
 import com.cooksys.social_media.dtos.TweetResponseDto;
 import com.cooksys.social_media.entities.Tweet;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring", uses = {UserMapper.class})
+@Mapper(componentModel = "spring", uses = {UserMapper.class},unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface TweetMapper {
 
     TweetResponseDto entityToDto(Tweet tweet);
--- src/main/java/com/cooksys/social_media/repositories/TweetRepository.java
+++ src/main/java/com/cooksys/social_media/repositories/TweetRepository.java
@@ -1,5 +1,8 @@
 package com.cooksys.social_media.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,9 @@ import com.cooksys.social_media.entities.Tweet;
 
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+
+	Optional<Tweet> findById(Long id);
+
+    List<Tweet> findAllByDeletedFalse();
 
 }
